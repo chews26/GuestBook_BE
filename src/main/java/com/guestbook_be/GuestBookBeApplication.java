46b211bb107993a1205b68daa60e8f1347b09f68
shine <112529297+chews26@users.noreply.github.com>
2025-08-20 01:28:14 +0900

    :sparkles: init : Initial commit(신규)

---
+++ src/main/java/com/guestbook_be/GuestBookBeApplication.java
@@ -0,0 +1,13 @@
+package com.guestbook_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestBookBeApplication.class, args);
+    }
+
+}
